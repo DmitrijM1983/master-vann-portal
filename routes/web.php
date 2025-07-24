a77433353fbce4ff0d6a774c99e880d25d98723e
DmitrijM1983 <dmitrij.m.183@yandex.ru>
2025-07-24 15:05:56 +0300

    web.php changed

--- routes/web.php
+++ routes/web.php
@@ -47,7 +47,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
         $request->fulfill();
 
-        return redirect()->route('profile');
+        return redirect()->route('profile', ['id' => $request->route('id')]);
     })->middleware('signed')->name('verification.verify');
 
     Route::post('/email/verification-notification', function (Request $request) {
